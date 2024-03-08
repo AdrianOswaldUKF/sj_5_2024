35199044d44b8cf85d671b78b55ca6081e4938d5
AdrianOswaldUKF <adrian.oswald@student.ukf.sk>
2024-03-08 13:52:45 +0100

    +Footer, index.php, portfolio.php

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
 <?php
-  include('partials/header.php');
+  include_once('partials/header.php');
 ?>
     <main>
       <section class="slides-container">
-        <div class="slide fade">
-          <img src="img/carousel/carousel.jpg">
-          <div class="slide-text">
-            Prvý nadpis
-          </div>
-        </div>
-        
-        <div class="slide fade">
-          <img src="img/carousel/carousel2.jpg">
-          <div class="slide-text">
-            Druhý nadpis
-          </div>
-        </div>
-        
-        <div class="slide fade">
-          <img src="img/carousel/carousel3.jpg">
-          <div class="slide-text">
-            Tretí nadpis
-          </div>
-        </div>
-        
+        <?php
+          $headings = array('Prvý nadpis','Druhý nadpis','Tretí nadpis');
+          $img_fodler = 'img/carousel/';
+          $img_files = glob($img_fodler.'*.jpg');
+
+          for($i = 0; $i < count($img_files); $i++) {
+            echo('<div class="slide fade">');
+            echo('<img src="'.$img_files[$i].'">');
+            echo('<div class="slide-text">');
+
+            //ak je počet nadpisov rovný počtu obrázkov
+            if ( count($headings) == count($img_files) ) {
+              //vypíšem i-ty obrázok
+              echo( $headings[$i] );
+            } else {
+              if ( $i < count ($headings) ) {
+                echo( $headings[$i] );
+                //inak nevypíšem nič
+              }
+            }
+            echo('</div>');
+            echo('</div>');
+          }
+        ?>
         <a id="prev" class="prev">❮</a>
         <a id="next" class="next">❯</a>
         
@@ -47,36 +50,9 @@
         </div>
       </section>
     </main>
-    
-  <footer class="container bg-dark text-white">
-    <div class="row">
-      <div class="col-25">
-        <h4>Kto sme</h4>
-        <p>Laboris duis ut est fugiat et reprehenderit magna labore aute.</p>
-        <p>Laboris duis ut est fugiat et reprehenderit magna labore aute.</p>
-        <p>Laboris duis ut est fugiat et reprehenderit magna labore aute.</p>
-      </div>
-      <div class="col-25 text-left">
-        <h4>Kontaktujte nás</h4>
-        <p><i class="fa fa-envelope" aria-hidden="true"><a href="mailto:livia.kelebercova@gmail.com"> livia.kelebercova@gmail.com</a></i></p>
-        <p><i class="fa fa-phone" aria-hidden="true"><a href="tel:0909500600"> 0909500600</a></i></p>
-      </div>
-      <div class="col-25">
-        <h4>Rýchle odkazy</h4>
-        <p><a href="/">Domov</a></p>
-        <p><a href="/qna">Q&A</a></p>
-        <p><a href="/kontakt">Kontakt</a></p>
-      </div>
-      <div class="col-25">
-        <h4>Nájdete nás</h4>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10614.839764656655!2d18.0910518!3d48.3084298!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xba2bad032d96b960!2sFakulta%20pr%C3%ADrodn%C3%BDch%20vied%20a%20informatiky!5e0!3m2!1ssk!2ssk!4v1669307792855!5m2!1ssk!2ssk" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
-       </div>
-    </div>
-    <div class="row">
-      Created and designed by Lívia
-    </div>
-  </footer>
-
+    <?php
+    include_once('partials/footer.php')
+    ?>
     <script src="js/menu.js"></script>
     <script src="js/slider.js"></script>
 </body>
